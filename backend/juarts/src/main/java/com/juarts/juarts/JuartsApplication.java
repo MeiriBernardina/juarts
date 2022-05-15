251d5d6b9ca63c768b98715d7e0c258ddf07c9b1
“MeiriBernardina” <meiri.umc@gmail.com>
2022-05-15 18:02:19 -0300

    projeto criado

---
+++ backend/juarts/src/main/java/com/juarts/juarts/JuartsApplication.java
@@ -0,0 +1,13 @@
+package com.juarts.juarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuartsApplication.class, args);
+	}
+
+}
---
+++ backend/juarts/src/test/java/com/juarts/juarts/JuartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juarts.juarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
