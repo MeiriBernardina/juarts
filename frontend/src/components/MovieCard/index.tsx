b4d65b6fb4b556222ff49a9993c67d09b7b9f789
“MeiriBernardina” <meiri.umc@gmail.com>
2022-05-22 22:58:48 -0300

    show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 
 
-function MovieCard( {movie}: Props) {
+function MovieCard( {movie} : Props) {
 
   return (
     <div>
@@ -18,7 +18,7 @@ function MovieCard( {movie}: Props) {
       />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score} />
 
         <Link to={`/form/${movie.id}`}>
             <div className="btn btn-primary dsmovie-btn">Avaliar</div>
